c86df8ca302a6e0f9e05e415083467fca6ee2662
Kyle Jones <kylej@democ.org>
2024-07-16 14:23:06 -0400

    Add redirect helper

--- src/boosted_impl.rs
+++ src/boosted_impl.rs
@@ -34,9 +34,9 @@ where
     }
 
     pub async fn redirect(
-        url: &str,
+        url: String,
         opt_boost_headers: Option<Vec<BoostHeader>>,
-    ) -> Result<Boosted<Option<&str>>> {
+    ) -> Result<Boosted<Option<String>>> {
         let url_str = url.to_string();
         let mut boost_headers = vec![BoostHeader::Location(url_str.clone())];
         if let Some(boost_header_vec) = opt_boost_headers {
@@ -44,7 +44,7 @@ where
                 boost_headers.push(boost_header);
             }
         }
-        Boosted::try_new(BoostedArgs::<Option<&str>> {
+        Boosted::try_new(BoostedArgs::<Option<String>> {
             title: "".to_string(),
             headers: HashMap::from([("Location".to_string(), url_str)]),
             boost_headers,
